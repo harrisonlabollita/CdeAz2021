0de228a98b8f6e2c232a7357926a52d7c7d0f610
harrisonlabollita <hlabollita0219@lions.piedmont.edu>
2021-06-22 16:12:56 -0700

    updates to notebooks

--- update.jl
+++ update.jl
@@ -1,6 +1,6 @@
 using IJulia
 
-function main()
+function main(type)
    println("Updating .ipynb notebooks to html slides...")
    for d in readdir()
        slides = d * "/slides/"
@@ -9,13 +9,29 @@ function main()
           curr_dir = readdir(notebooks)
           for note in curr_dir
               if occursin("ipynb", note)
-	         println("Converting $note to html")
-		 run(`jupyter nbconvert $(joinpath(notebooks * note)) --to slides`)
-		 run(`mv $(joinpath(notebooks * note[1:end-6] * ".slides.html")) $slides`)
+		 if type == "html"
+	            try
+		      println("Converting $note to html")
+	              run(`jupyter nbconvert $(joinpath(notebooks * note)) --to slides`)
+	              run(`mv $(joinpath(notebooks * note[1:end-6] * ".slides.html")) $slides`)
+		    catch
+		      println("Something went wrong with $note")
+	            end
+	    	 elseif type == "pdf"
+	            try
+		      println("Converting $note to pdf")
+		      run(`jupyter nbconvert $(joinpath(notebooks * note)) --to pdf`)
+		      run(`mv $(joinpath(notebooks * note[1:end-6] * ".pdf")) $slides`)
+		    catch
+		      println("Something went wrong with $note")
+	            end
+		    println("Converting $note to pdf")
+		 else
+		    error("Unknown type $(type)")
+		 end
 	      end
 	  end
         end
     end
 end
-
-main()
+main(ARGS[1])
